97573542747da54db2693eb1eca9c1d6c57a34d2
Stanton <86133203+aerotron1@users.noreply.github.com>
2021-12-04 15:02:15 +0530

    Fixed watched

--- src/AddMovie.js
+++ src/AddMovie.js
@@ -7,7 +7,7 @@ function AddMovie(props) {
             <Image src="https://hub.dummyapis.com/Image?text=Item&height=120&width=120" roundedCircle />
             <h2 style={{ color: props.addMovie.watched ? "green" : "grey" }}>Name: {props.addMovie.name}</h2>
             <h3 style={{ color: props.addMovie.watched ? "green" : "grey" }}>Release: {props.addMovie.release}</h3>
-            <Button variant="outline-success" onClick={() => props.watchedMovie(props.addMovie.name)}>Watched</Button>{' '}
+            <Button variant="outline-success" onClick={() => props.watchedMovie(props.addMovie.name, props.addMovie.release)}>Watched</Button>{' '}
             <Button variant="outline-warning" onClick={() => props.removeMovie(props.addMovie)}>Remove</Button>{' '}
             <div contenteditable="true"><Button variant="outline-danger" onClick={() => props.watchedMovie(props.addMovie)}>Edit</Button>{' '}</div>
         </div>
